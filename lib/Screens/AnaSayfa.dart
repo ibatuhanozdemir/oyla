14aa4156c00bba1548d406a79f2d72c5a51b1d8e
Tayfun <vuraltayfun93@gmail.com>
2020-11-22 16:54:44 +0300

    fdg

--- lib/Screens/AnaSayfa.dart
+++ lib/Screens/AnaSayfa.dart
@@ -1,14 +1,31 @@
-import 'package:flutter/material.dart';
-import 'package:oyla/Backend/authentication.dart';
+import 'dart:convert';
 
-class AnaSayfa extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:oyla/Backend/Kategoriler.dart';
+import 'package:oyla/Backend/authentication.dart';
+import 'package:http/http.dart' as http;
+class AnaSayfa extends StatefulWidget {
+  @override
+  _AnaSayfaState createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa> {
+  List KategorilerListesi=[];
 
   AuthService authService = AuthService();
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
+  return KategorilerListesi.isEmpty?
+
+  Container():
+  Scaffold(
       endDrawer: Drawer(),
       appBar: AppBar(
         centerTitle: true,
@@ -16,21 +33,26 @@ class AnaSayfa extends StatelessWidget {
       ),
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              Container(
-                color: Colors.white,
-                child: RaisedButton(
-                  child: Text("sign out"),
-                  onPressed: (){
-                    authService.signOut();
-                  },
-                ),
-              ),
-            ],
+          child: ListView.builder(itemBuilder: (_, index){
+            return Text(KategorilerListesi[index]['kategori_isim']) ;
+          },
+            itemCount: KategorilerListesi.length,
           ),
         ),
       ),
     );
   }
+    Future getData() async{
+      var url='http://www.taybtu.com/get.php?isim=ali';
+      http.Response response = await http.get(url);
+      setState(() {
+        KategorilerListesi = jsonDecode(response.body);
+        print(KategorilerListesi[2]['kategori_isim']);
+
+      });
+
+
+
+
+    }
 }
