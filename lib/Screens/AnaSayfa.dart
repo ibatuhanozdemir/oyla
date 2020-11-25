c61405538c77b0ce022d4acfc06b33e968ff729d
ibatuhanozdemir <ismailbatuhanozdemir@yandex.com>
2020-11-25 17:09:13 +0300

    juyr

--- lib/Screens/AnaSayfa.dart
+++ lib/Screens/AnaSayfa.dart
@@ -1,69 +1,106 @@
 import 'dart:convert';
 
-import 'package:emojis/emojis.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:oyla/Backend/Kategoriler.dart';
 import 'package:oyla/Backend/authentication.dart';
 import 'package:http/http.dart' as http;
+import 'package:oyla/Screens/Kal%C4%B1pWidgetlar/Drawer.dart';
+
 class AnaSayfa extends StatefulWidget {
   @override
   _AnaSayfaState createState() => _AnaSayfaState();
 }
 
 class _AnaSayfaState extends State<AnaSayfa> {
-  List KategorilerListesi=[];
+  List KategorilerListesi = [
+    "Ürünler",
+    "Kişiler",
+    "Yerler",
+    "Düşünceler",
+  ];
+  List ikonlarListesi = [
+    "assignment",
+    "assignment",
+    "assignment",
+    "assignment",
+  ];
+  List altKategoriChecker = ["1", "0", "1", "0"];
 
   AuthService authService = AuthService();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getData();
+    //getData();
   }
+
   @override
   Widget build(BuildContext context) {
-
-  return KategorilerListesi.isEmpty?
-
-  Container():
-  Scaffold(
-      endDrawer: Drawer(child:RaisedButton(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(24))),
-        color: Colors.red,
-        textColor: Colors.white,
-        onPressed: (){
-          FirebaseAuth.instance.signOut();
-        },
-        child: Text("${Emojis.door} Çıkış Yap"),
-      )),
+    return Scaffold(
+      endDrawer: KalipDrawer(),
       appBar: AppBar(
         centerTitle: true,
-        title: Text("deneme"),
+        title: Text("Ana Kategoriler"),
       ),
       body: SafeArea(
         child: Center(
-          child: ListView.builder(itemBuilder: (_, index){
-            return Text(KategorilerListesi[index]['kategori_isim']) ;
-          },
+          child: ListView.builder(
+            itemBuilder: (_, index) {
+              return Card(
+                  child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("100"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(Icons.assignment),
+                        Text(KategorilerListesi[index]),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Oy ver",style: TextStyle(color: Colors.blue.shade700),),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        altKategoriChecker[index] == "1"
+                            ? Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.red,
+                                size: 30,
+                              )
+                            : Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.black26,
+                                size: 30,
+                              ),
+                      ],
+                    ),
+                  ],
+                ),
+              ));
+            },
             itemCount: KategorilerListesi.length,
           ),
         ),
       ),
     );
   }
-    Future getData() async{
-      var url='http://www.taybtu.com/get.php';
-      http.Response response = await http.get(url);
-      setState(() {
-        KategorilerListesi = jsonDecode(response.body);
-        print(KategorilerListesi[2]['kategori_isim']);
 
-      });
+/*
+  Future getData() async {
+    var url = 'http://www.taybtu.com/get.php?isim=ali';
+    http.Response response = await http.get(url);
+    setState(() {
+      KategorilerListesi = jsonDecode(response.body);
+      print(KategorilerListesi[2]['kategori_isim']);
+    });
+  }
+  */
 
-
-
-
-    }
 }
--- lib/Screens/KalıpWidgetlar/Drawer.dart
+++ lib/Screens/KalıpWidgetlar/Drawer.dart
@@ -1,8 +1,77 @@
+
+import 'package:emojis/emojis.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class KalipDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: ListView(
+        children: [
+          Column(
+            children: [
+              _createHeader(),
+              _createDrawerItem(text: "Neleri Takip Ediyorum?",onTap: (){
+              }, icon: Icons.assignment)
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.45,
+          ),
+          Column(
+            children: [
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(24))),
+                color: Colors.red,
+                textColor: Colors.white,
+                onPressed: (){
+                  FirebaseAuth.instance.signOut();
+                },
+                child: Text("${Emojis.door} Çıkış Yap"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _createHeader() {
+    return DrawerHeader(
+        margin: EdgeInsets.zero,
+        padding: EdgeInsets.zero,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                    "images/business-163464_1280.jpg"))),
+        child: Stack(children: <Widget>[
+          Positioned(
+              bottom: 12.0,
+              right: 20,
+              child: Text("Seçenekler",
+                  style: TextStyle(
+                      color: Colors.orange.shade900,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500))),
+        ]));
+  }
+
+  Widget _createDrawerItem(
+      {IconData icon, String text, GestureTapCallback onTap}) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(icon),
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(text),
+          )
+        ],
+      ),
+      onTap: onTap,
+    );
   }
 }
